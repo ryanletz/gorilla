a23e5e10cc3513b796aa6ec03e3771c1bdc10fbf
Ryan Letz <ryan.letz@gmail.com>
2013-03-11 02:26:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gorilla::Application.config.secret_token = '0eaa6eb6a27a62973ec24a5f66b424c598ff4be60961fb90622c9905dd03370b12ba621a424360f93ea70f0494a0d187032146368649fcfe36e09f9c19a2677e'
